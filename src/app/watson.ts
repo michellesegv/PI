e4103239f54403a048f5e7fecc5c938818466627
LuRod <EXPERIS-Ana.Rodriguez@spsa.pe>
2020-11-24 23:35:11 -0500

    script watson

--- src/app/watson.ts
+++ src/app/watson.ts
@@ -1,1 +1,13 @@
 console.log('wiwi watson')
+window.watsonAssistantChatOptions = {
+  integrationID: "0e38af37-aa0d-4ffb-a18f-c38d1f0e22f7", // The ID of this integration.
+  region: "us-south", // The region your integration is hosted in.
+  serviceInstanceID: "98408b3d-e222-49eb-8956-d42fe3b69e75", // The ID of your service instance.
+  onLoad: function(instance) { instance.render(); }
+};
+setTimeout(function(){
+const t=document.createElement('script');
+t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+document.head.appendChild(t);
+});
+
